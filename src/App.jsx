3c8813d7da1e18fab5afc4bae390e115b1c1c436
manprajapatie <manprajapatie@gmail.com>
2025-01-02 22:28:42 +0530

    add react router dom and create error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home' 
+import Success from './pages/Success'
 
 function App() {
   return (
     <>
-        <h1>My Name</h1>
+        <BrowserRouter>
+
+        //Outlet add nahi kiya he isliye Routes ke sath handle kar rhe he / Outlet handle kiya hota to Route ke sath hi handle karna padta
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='success' element={<Success/>}/>
+          <Route path='success' element={<Success/>}/>
+        </Routes>
+        </BrowserRouter>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+        <h1>HOme</h1>
+    </>
   )
 }
 
