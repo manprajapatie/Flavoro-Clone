15d7ae7f2d2e0e828bc1cb64f27c61284a1eb034
manprajapatie <manprajapatie@gmail.com>
2025-01-02 23:01:56 +0530

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home' 
 import Success from './pages/Success'
+import Error from './pages/Error'
 
 function App() {
   return (
     <>
         <BrowserRouter>
 
-        //Outlet add nahi kiya he isliye Routes ke sath handle kar rhe he / Outlet handle kiya hota to Route ke sath hi handle karna padta
+        {/* Outlet add nahi kiya he isliye Routes ke sath handle kar rhe he  Outlet handle kiya hota to Route ke sath hi handle karna padta */}
+
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='success' element={<Success/>}/>
-          <Route path='success' element={<Success/>}/>
+          <Route path='/*' element={<Error/>}/>
         </Routes>
         </BrowserRouter>
     </>
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Error() {
   return (
     <div>
-      Error
+      <h1>Error</h1>
     </div>
   )
 }
